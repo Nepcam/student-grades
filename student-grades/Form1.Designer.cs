7cab0adb63d14952c09db772d26bf22dbfd9f2f3
Nepcam <cameron.nepe@gmail.com>
2020-06-12 12:22:57 +1200

    (load marks button) pseudo code and variables

--- student-grades/Form1.Designer.cs
+++ student-grades/Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.listBoxDisplay = new System.Windows.Forms.ListBox();
+            this.pictureBoxGraph = new System.Windows.Forms.PictureBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadMarksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graphMarksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(941, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // listBoxDisplay
+            // 
+            this.listBoxDisplay.FormattingEnabled = true;
+            this.listBoxDisplay.Location = new System.Drawing.Point(665, 26);
+            this.listBoxDisplay.Name = "listBoxDisplay";
+            this.listBoxDisplay.Size = new System.Drawing.Size(276, 550);
+            this.listBoxDisplay.TabIndex = 1;
+            // 
+            // pictureBoxGraph
+            // 
+            this.pictureBoxGraph.Location = new System.Drawing.Point(0, 27);
+            this.pictureBoxGraph.Name = "pictureBoxGraph";
+            this.pictureBoxGraph.Size = new System.Drawing.Size(659, 536);
+            this.pictureBoxGraph.TabIndex = 2;
+            this.pictureBoxGraph.TabStop = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadMarksToolStripMenuItem,
+            this.graphMarksToolStripMenuItem,
+            this.clearGraphToolStripMenuItem,
+            this.reportToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadMarksToolStripMenuItem
+            // 
+            this.loadMarksToolStripMenuItem.Name = "loadMarksToolStripMenuItem";
+            this.loadMarksToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadMarksToolStripMenuItem.Text = "Load Mark File";
+            this.loadMarksToolStripMenuItem.Click += new System.EventHandler(this.loadMarksToolStripMenuItem_Click);
+            // 
+            // graphMarksToolStripMenuItem
+            // 
+            this.graphMarksToolStripMenuItem.Name = "graphMarksToolStripMenuItem";
+            this.graphMarksToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.graphMarksToolStripMenuItem.Text = "Graph Marks";
+            // 
+            // clearGraphToolStripMenuItem
+            // 
+            this.clearGraphToolStripMenuItem.Name = "clearGraphToolStripMenuItem";
+            this.clearGraphToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clearGraphToolStripMenuItem.Text = "Clear Graph";
+            this.clearGraphToolStripMenuItem.Click += new System.EventHandler(this.clearGraphToolStripMenuItem_Click);
+            // 
+            // reportToolStripMenuItem
+            // 
+            this.reportToolStripMenuItem.Name = "reportToolStripMenuItem";
+            this.reportToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.reportToolStripMenuItem.Text = "Report";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(941, 575);
+            this.Controls.Add(this.pictureBoxGraph);
+            this.Controls.Add(this.listBoxDisplay);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ListBox listBoxDisplay;
+        private System.Windows.Forms.PictureBox pictureBoxGraph;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadMarksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem graphMarksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearGraphToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- student-grades/Form1.cs
+++ student-grades/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,64 @@ namespace student_grades
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //The width of a bar in the bar graph
+        const int BAR_WIDTH = 25;
+
+        private void clearGraphToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBoxGraph.Refresh();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Draws a vertical bar that is part of a bar graph.
+        /// i.e. It fills a rectangle at position (x,y) with the specified colour.
+        /// Then draws a black outline for the rectangle.
+        /// Uses the BAR_WIDTH constant for the size of the rectangle.
+        /// </summary>
+        /// <param name="paper">The Graphics object to draw on.</param>
+        /// <param name="x">The x position of the top left corner of the rectangle.</param>
+        /// <param name="y">The y position of the top left corner of the rectangle.</param>
+        /// <param name="colour">The colour to fill the background of the rectangle with.</param>
+        private void DrawABar(Graphics paper, int x, int y, int length, Color colour)
+        {
+            //create a brush of specified colour and fill background with this colour 
+            SolidBrush brush = new SolidBrush(colour);
+            paper.FillRectangle(brush, x, y, BAR_WIDTH, length);
+
+            //draw outline in black
+            paper.DrawRectangle(Pens.Black, x, y, BAR_WIDTH, length);
+        }
+
+        private void loadMarksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Set filter of dialog control
+            const string FILTER = "CSV FIles|*.csv|ALL Files|*.*";
+            //GET the file reader 
+            StreamReader reader;
+
+            string line = "", objectType = "";
+            // SET the array variables
+            string[] idArray;
+            string[] marksArray;
+
+            List<int> id = new List<int>();
+            List<int> mark = new List<int>()
+
+                //IF(user selects a file) THEN
+                    //Open input file
+                    //WHILE not end of file
+                    //Your pseudo - code goes here
+                    //ENDWHILE
+                    //Close file
+                    //Display message that file loaded
+                //ENDIF
         }
     }
 }
